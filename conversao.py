9a9b29c8e550803a8fefbe26b1e113f88b1a2793
Yuri <capic.2022118isinf0381@aluno.ifpi.edu.br>
2025-05-11 22:47:02 -0300

    :rocket: Adicionando a pasta no github

---
+++ conversao.py
@@ -0,0 +1,6 @@
+valor_em_dolar = float(input("Digite aqui o valor em dólar: "))
+cotacao_do_dolar = 6
+
+conversao_dolar_real = valor_em_dolar* cotacao_do_dolar
+
+print("O valor {} dólares convertido para real fica {}".format(valor_em_dolar, conversao_dolar_real))
