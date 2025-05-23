c6d016f7fb328968baf32ff1aac0da3f31868e63
Yuri <capic.2022118isinf0381@aluno.ifpi.edu.br>
2025-05-23 15:27:43 -0300

    :rocket: Atualizando pasta

---
+++ Algoritmos/aula6.py
@@ -0,0 +1,46 @@
+# Soma
+n1, n2, n3 = 2, 4, 6
+n1 = n2 + n3
+print(n1)
+print("\n")
+# Subtração
+x1, x2, x3 = 5, 4, 3
+x1 = x2 - x3
+print(x1)
+print("\n")
+# Divisão
+y1, y2, y3 = 100, 8, 2
+y1 = y2/y3
+print(y1)
+print("\n")
+# Multiplicação
+m1, m2, m3 = 500, 2, 5
+m1 = m2 * m3
+print(m1)
+print("\n")
+# Módulo
+a1, a2, a3 = 80000, 5, 2
+a1 = a2 % a3 
+print(a1)
+print("\n")
+# Menor que
+x, y = 5, 10
+print(x < y)
+print("\n")
+# Menor ou igual a
+x, y, z= 5, 10, 5
+print(x<=y)
+print(x<=z)
+print("\n")
+#Maior que
+j, h = 10, 12
+print(j > h)
+print(j >= h)
+print(j == h)
+print("\n")
+
+# AND
+x, y = True, False
+z = False
+print(x & y)
+print(y & z)
